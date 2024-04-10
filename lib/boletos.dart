86a238743b4a2251fd935a4227434f45355e408d
REYNA_LETICIA_MOLINA_VILLELA <a.21308051280967@cbtis128.edu.mx>
2024-04-10 16:13:09 +0000

    actualizado

---
+++ lib/boletos.dart
@@ -0,0 +1,48 @@
+import 'package:molinadrawerv2/drawer_menu.dart';
+import 'package:flutter/material.dart';
+
+class Boletos extends StatelessWidget {
+  static const String routeName = '/boletos';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "Molina Inicio",
+          style: TextStyle(
+            fontSize: 18,
+            color: Color(0xffffffff),
+          ),
+        ),
+        backgroundColor: Color(0xff700e0e),
+      ),
+      drawer: DrawerMenu(),
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        mainAxisSize: MainAxisSize.max,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Container(
+            padding: const EdgeInsets.all(0.0),
+            color: Colors.cyanAccent,
+            width: 80.0,
+            height: 80.0,
+          ),
+          Container(
+            padding: const EdgeInsets.all(0.0),
+            color: Colors.blueAccent,
+            width: 80.0,
+            height: 80.0,
+          ),
+          Container(
+            padding: const EdgeInsets.all(0.0),
+            color: Colors.orangeAccent,
+            width: 80.0,
+            height: 80.0,
+          ),
+        ],
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
